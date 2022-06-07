349730aed9beb8e28733a1bf16345fd44872743f
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-06 22:37:43 -0700

    Added GET request to fetch all users

--- api/server.js
+++ api/server.js
@@ -2,6 +2,20 @@
 
 const express = require('express');
 
+const Users = require('./users/model');
+
 const server = express();
+
+server.get('/api/users', (request, response) => {
+    Users.find()
+        .then(users => {
+            response.json(users);
+        })
+        .catch(error => {
+            response.status(500).json({
+                message: 'The users information could not be retrieved'
+            })
+        })
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
