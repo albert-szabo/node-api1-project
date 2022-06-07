5f6c73823b2222619c25cfe90299bfed101cba3b
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-07 00:01:09 -0700

    Added PUT request to update a specific user

--- api/server.js
+++ api/server.js
@@ -75,4 +75,29 @@ server.delete('/api/users/:id', async (request, response) => {
     }
 });
 
+server.put('/api/users/:id', async (request, response) => {
+    const possibleUser = await Users.findById(request.params.id);
+    if (!possibleUser) {
+        response.status(404).json({
+            message: 'The user with the specified ID does not exist'
+        });
+    } else {
+        if (!request.body.name || !request.body.bio) {
+            response.status(400).json({
+                message: 'Please provide name and bio for the user'
+            });
+        } else {
+            Users.update(request.params.id, request.body)
+                .then(updatedUser => {
+                    response.status(200).json(updatedUser);
+                })
+                .catch(error => {
+                    response.status(500).json({
+                        message: 'The user information could not be modified'
+                    });
+                });
+        }
+    }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
