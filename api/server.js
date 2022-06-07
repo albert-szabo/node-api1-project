0d6ded243f72b331fbefe28da803c224dd9acfff
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-06 23:07:50 -0700

    Added POST request to add / create a new user

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ const Users = require('./users/model');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/api/users', (request, response) => {
     Users.find()
         .then(users => {
@@ -33,6 +35,25 @@ server.get('/api/users/:id', (request, response) => {
                 message: 'The user information could not be retrieved'
             });
         });
+});
+
+server.post('/api/users', (request, response) => {
+    const user = request.body;
+    if (!user.name || !user.bio) {
+        response.status(400).json({
+            message: 'Please provide name and bio for the user'
+        });
+    } else {
+        Users.insert(user)
+        .then(newUser => {
+            response.status(201).json(newUser);
+        })
+        .catch(error => {
+            response.status(500).json({
+                message: 'There was an error while saving the user to the database'
+            });
+        });
+    }
 });
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
